8a0a70731b9768193c51ad997cbf58d8b4eeba33
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-12 23:53:00 -0400

    myAge, herAge

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,9 +3,8 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-	    int age = 30;
-	    int temperature = 20;
-	    age = 35;
-	    System.out.println(age);
+	    int myAge = 30;
+	    int herAge = myAge;
+	    System.out.println(herAge);
     }
 }
