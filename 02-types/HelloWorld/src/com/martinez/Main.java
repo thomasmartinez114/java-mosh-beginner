1b6acb9a1a1781e2f0b993e77430913d726057de
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:02:35 -0400

    storing whole numbers with Long - can use _ to make number easier to read

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-	    int myAge = 30;
-	    int herAge = myAge;
-	    System.out.println(herAge);
+	    byte myAge = 30;
+	    long viewsCount = 3_123_456_789L;
     }
 }
