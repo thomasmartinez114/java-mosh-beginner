47acb2fce28e08f74898106c0b261f28560ba10b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:43:51 -0400

    referenced type copied by their refernces

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -1,13 +1,13 @@
 package com.martinez;
 
-import java.util.Date;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        byte x = 1;
-        byte y = x;
-        x = 2;
-        System.out.println(y);
+        Point point1 = new Point(x:1, y:1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
