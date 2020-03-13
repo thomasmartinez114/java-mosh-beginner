00369b9a25e5645695ba1063e396a0be7639dc11
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:46:36 -0400

    print out string

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -1,13 +1,9 @@
 package com.martinez;
 
-import java.awt.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(x:1, y:1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World";
+        System.out.println(message);
     }
 }
