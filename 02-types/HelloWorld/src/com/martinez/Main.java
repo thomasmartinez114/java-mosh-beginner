40af4d4c022d9a6ab6aaad7f00d37841d85b6b52
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:54:27 -0400

    trim() to remove the white space in the string

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
-        System.out.println(message.toLowerCase());
+        String message = "   Hello World" + "!!   ";
+        System.out.println(message.trim());
     }
 }
