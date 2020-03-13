c68be5697ced3b73b04cd46779641b33033fb5b6
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:29:22 -0400

    imported java.util.Date - to use Date

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -1,12 +1,11 @@
 package com.martinez;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	    byte myAge = 30;
-	    long viewsCount = 3_123_456_789L;
-	    float price = 10.99F;
-	    char letter = 'A';
-	    boolean isEligible = true;
+        byte age = 30;
+        Date
     }
 }
