fb985c0e32a836bb99f484b62f7a99438d77813b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:48:28 -0400

    startsWith

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
     }
 }
