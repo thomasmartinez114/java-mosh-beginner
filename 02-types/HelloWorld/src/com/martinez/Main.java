47d3d270ed69b7aff1df9d8f66f7697d6eda1740
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 00:53:13 -0400

    toLowerCase()

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.replace("!", "*"));
+        System.out.println(message.toLowerCase());
     }
 }
