ff34ee53ae24937771f9ebe063a09c177b8135ce
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 15:23:25 -0400

    multi demensional arrays - showing the array by using Arrays.deepToString(numbers)

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = { 2, 3, 5, 1, 4 };
-        System.out.println(numbers.length);
-
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
