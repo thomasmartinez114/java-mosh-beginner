af54c739989155a8827b3fd0d8b092dc31a77485
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 19:14:38 -0400

    Converting string to int - Integer.parseInt(x)

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // Implicit casting
         // byte > short > int > long > float > double
-        double x = 1.1;
-        int y = (int)x + 2;
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
         System.out.println(y);
     }
 }
