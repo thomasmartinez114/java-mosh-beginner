979c46f9f1b140ce42a292e6c322be0918f6ce1c
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 22:40:38 -0400

    making arrays - new int[5] means how many numbers will be in the array

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,6 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        // c:\Windows\...
-        String message = "c:\tWindows\\..";
-        System.out.println(message);
+        int[] numbers = new int[5];
     }
 }
