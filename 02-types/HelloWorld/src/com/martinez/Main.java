db0d32b7d86342934c8e953963e3c67d2fcddf9b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 13:20:01 -0400

    setting the array indexes using numbers = { 2, 3, 4, 5, 6 }

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
+        int[] numbers = { 2, 3, 5, 1, 4 };
+        System.out.println(numbers.length);
+
         System.out.println(Arrays.toString(numbers));
     }
 }
