79a10b0caad1a96030aeaf5691a8862c3f7ee7f3
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 18:59:00 -0400

    adding

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -1,10 +1,9 @@
 package com.martinez;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        final float PI = 3.14F;
+        int result = 10 + 3;
+        System.out.println(result);
     }
 }
