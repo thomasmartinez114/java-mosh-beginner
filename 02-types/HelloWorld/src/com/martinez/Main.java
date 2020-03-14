6a48ba8f41f6d2eb1719d3bafbe70842814b27b1
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 15:28:09 -0400

    curly braces for multi dimensioanl array

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[][][] numbers = new int[2][3][5];
-        numbers[0][0] = 1;
+        int[][] numbers = { { 1, 2, 3}, { 4, 5, 6} };
         System.out.println(Arrays.deepToString(numbers));
     }
 }
