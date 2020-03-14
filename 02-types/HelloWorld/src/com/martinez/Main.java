aa38503f6b355a35712a31906f3a8eadf119f14c
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 15:26:59 -0400

    3 dimensional array

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = new int[2][3];
+        int[][][] numbers = new int[2][3][5];
         numbers[0][0] = 1;
         System.out.println(Arrays.deepToString(numbers));
     }
