ace75f53493428e524a6580fb071e79538998578
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 19:35:26 -0400

    Math Class = .round()

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,10 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        // Implicit casting
-        // byte > short > int > long > float > double
-        String x = "1";
-        int y = Integer.parseInt(x) + 2;
-        System.out.println(y);
+        int result = Math.round(1.1F);
+        System.out.println(result);
     }
 }
