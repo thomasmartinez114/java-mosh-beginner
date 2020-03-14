ecacb6283d04aa9fd82335cfab71ca083b48431b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 19:01:33 -0400

    incrementing

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,8 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        double result = (double)10 / (double)3;
-        System.out.println(result);
+        int x = 1;
+        x++;
+        System.out.println(x);
     }
 }
