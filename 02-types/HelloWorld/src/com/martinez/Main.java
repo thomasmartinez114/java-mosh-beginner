0b9f6c9a444b78e7a1fd44f7bc6bb3eb21abe041
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-13 21:09:34 -0400

    \n - new line

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // c:\Windows\...
-        String message = "c:\\Windows\\..";
+        String message = "c:\nWindows\\..";
         System.out.println(message);
     }
 }
