ddf2e2d7f9eb361f2eee82d0bbf0121bb0f4e05f
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 19:12:32 -0400

    Explicit Casting - setting x to int

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // Implicit casting
-        // byte > short > int > long
-        short x = 1;
-        int y = x + 2;
-
+        // byte > short > int > long > float > double
+        double x = 1.1;
+        int y = (int)x + 2;
         System.out.println(y);
     }
 }
