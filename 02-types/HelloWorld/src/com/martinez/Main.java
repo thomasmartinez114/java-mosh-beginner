bff08fb316205c9bb8eb30729878fff90f1618fd
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 20:59:05 -0400

    Formatting to Percent - getPercentInstace()

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,8 +5,8 @@ import java.text.NumberFormat;
 public class Main {
 
     public static void main(String[] args) {
-       NumberFormat currency = NumberFormat.getCurrencyInstance();
-       String result = currency.format(1234567.891);
+       NumberFormat percent = NumberFormat.getPercentInstance();
+       String result = percent.format(0.1);
         System.out.println(result);
     }
 }
