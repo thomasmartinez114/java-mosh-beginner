0fbf7dfecb638716ae0dd467a819219bfba6062e
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 20:40:16 -0400

    Math Class = two get rid of the decimal place

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        double result = Math.random() * 100;
+        int result = (int) Math.round(Math.random() * 100);
         System.out.println(result);
     }
 }
