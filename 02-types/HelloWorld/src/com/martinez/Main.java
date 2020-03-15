cb31f3873110cecc30e57e50dd4504153d16a0fc
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 20:38:33 -0400

    Math.random() * 100 = ##.####

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        double result = Math.random();
+        double result = Math.random() * 100;
         System.out.println(result);
     }
 }
