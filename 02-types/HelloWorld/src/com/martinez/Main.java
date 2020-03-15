7865d46934e74b8ca84379547b7f2483f678dece
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 21:00:27 -0400

    Method Chaining to refactor code

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -5,8 +5,7 @@ import java.text.NumberFormat;
 public class Main {
 
     public static void main(String[] args) {
-       NumberFormat percent = NumberFormat.getPercentInstance();
-       String result = percent.format(0.1);
+       String result = NumberFormat.getPercentInstance().format(0.1);
         System.out.println(result);
     }
 }
