5a520292179c42109f54ae83c01e7238900e877a
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 23:30:16 -0400

    began MortageCalculator

---
+++ 02-types/MortageCalculator/src/com/martinez/Main.java
@@ -0,0 +1,28 @@
+package com.martinez;
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Ask user for:
+        // Principal
+        // Annual Interest Rate (divide by 100 then divide by 12) to get monthly interest
+        // Period (Years)
+        // Return Mortage rate (monthly): $###.##
+        // Formula used is:
+        // M = (P(r(1+r)^n)/((1+r)^n)-1)
+
+        // Static inputs for now then test
+        // M = ?
+        // P = $100,000
+        // r = 5%
+        // n =
+
+        
+
+
+    }
+}
