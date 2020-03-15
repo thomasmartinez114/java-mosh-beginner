b8427bdd9fe824eda4efeb7f70c8d4796b2e7e73
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-14 21:20:51 -0400

    User input - trimming the blank spaces

--- 02-types/HelloWorld/src/com/martinez/Main.java
+++ 02-types/HelloWorld/src/com/martinez/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        System.out.print("Name: ");
-       String name = scanner.nextLine();
+       String name = scanner.nextLine().trim();
        System.out.println("You are " + name);
     }
 }
