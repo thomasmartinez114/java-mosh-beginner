ace0c6e2a8d24db24adcf478649701e7d13db229
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-15 15:12:23 -0400

    Logical Operators - OR

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,12 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-	    int temperature = 12;
-	    boolean isWarm = temperature > 20 && temperature < 30;
-		System.out.println(isWarm);
+//	    int temperature = 12;
+////	    boolean isWarm = temperature > 20 && temperature < 30;
+////		System.out.println(isWarm);
+
+		boolean hasHighIncome = true;
+		boolean hasGoodCredit = true;
+		boolean isEligible = hasHighIncome || hasGoodCredit;
     }
 }
