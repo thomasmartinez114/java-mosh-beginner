f481896c7d6cdc6e987c38b39a83c0384dc35adb
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-15 15:10:12 -0400

    Logical Operators

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,8 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-	    int x = 1;
-	    int y = 1;
-	    System.out.println(x == y);
+	    int temperature = 12;
+	    boolean isWarm = temperature > 20 && temperature < 30;
+		System.out.println(isWarm);
     }
 }
