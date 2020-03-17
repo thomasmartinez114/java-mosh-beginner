d2b8417b2778208d605a7edbec857dfc5e6fc9a7
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-16 23:19:54 -0400

    Switch Statements - Switch

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		String role = "admin";
-		if (role == "admin")
-			System.out.println("You are an admin");
-		else if (role == "moderator")
-			System.out.println("You are a moderator");
-		else
-			System.out.println("You are a guest");
+
+		switch (role) {
+			case "admin":
+				System.out.println("You are an admin");
+				break;
+
+			case "moderator":
+				System.out.println("You are a moderator");
+				break;
+
+			default:
+				System.out.println("You are a guest");
+		}
     }
 }
