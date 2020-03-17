cff19bd3564d68be701a37ea9f175cf57466a756
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-16 21:55:27 -0400

    If Statements - make it easier to read

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -16,10 +16,11 @@ public class Main {
 		if (temp > 30) {
 			System.out.println("It's a hot day");
 			System.out.println("Drink water");
-		} else if (temp > 20 && temp <= 30) {
-			System.out.println("Beautiful day");
-		} else {
-			System.out.println("Cold day");
 		}
+		else if (temp > 20)
+			System.out.println("Beautiful day");
+		else
+			System.out.println("Cold day");
+
     }
 }
