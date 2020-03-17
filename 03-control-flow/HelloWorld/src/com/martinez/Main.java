72738f07113ba43a7e4f7c38e8616ad4d5dc192a
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-16 21:51:41 -0400

    If Statements - temp > 30

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -7,9 +7,13 @@ public class Main {
 ////	    boolean isWarm = temperature > 20 && temperature < 30;
 ////		System.out.println(isWarm);
 
-		boolean hasHighIncome = false;
-		boolean hasGoodCredit = true;
-		boolean hasCriminalRecord = false;
-		boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+//		boolean hasHighIncome = false;
+//		boolean hasGoodCredit = true;
+//		boolean hasCriminalRecord = false;
+//		boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+		int temp = 32;
+		if (temp > 30)
+			System.out.println("It's a hot day");
     }
 }
