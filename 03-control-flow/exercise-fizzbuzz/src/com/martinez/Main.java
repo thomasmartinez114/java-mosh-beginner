20f092f431ba55d7319d590d6985dfe34697e248
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-16 23:23:17 -0400

    began exercise

---
+++ 03-control-flow/exercise-fizzbuzz/src/com/martinez/Main.java
@@ -0,0 +1,12 @@
+package com.martinez;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // enter a number
+        // if number is diviisble by 5 = Fizz
+        // if number is divisible by 3 = buzz
+        // if number is divisible by 3 or 5 = Fizzbuzz
+        // if number isn't divisible by either get the number back
+    }
+}
