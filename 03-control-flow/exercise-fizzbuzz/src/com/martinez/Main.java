35a3db682ceee0f034ccb1583ba33f6bed058604
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-17 23:42:41 -0400

    Exercise - Scanner input

--- 03-control-flow/exercise-fizzbuzz/src/com/martinez/Main.java
+++ 03-control-flow/exercise-fizzbuzz/src/com/martinez/Main.java
@@ -9,6 +9,7 @@ public class Main {
         // if number is divisible by 3 or 5 = Fizzbuzz
         // if number isn't divisible by either get the number back
 
-        int test 12
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
     }
 }
