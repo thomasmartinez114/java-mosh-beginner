d9d00b13474c1c425046a6b7db4f74efbfb15265
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-19 19:51:51 -0400

    Loops - For

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -3,19 +3,7 @@ package com.martinez;
 public class Main {
 
 	public static void main(String[] args) {
-		String role = "admin";
-
-		switch (role) {
-			case "admin":
-				System.out.println("You are an admin");
-				break;
-
-			case "moderator":
-				System.out.println("You are a moderator");
-				break;
-
-			default:
-				System.out.println("You are a guest");
-		}
+		for (int i = 0; i < 5; i++)
+			System.out.println("Hello World");
     }
 }
