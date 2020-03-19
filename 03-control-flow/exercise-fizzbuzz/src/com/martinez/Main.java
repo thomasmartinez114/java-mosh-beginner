870849dd1f5443a35ca9358ae613b6b2bfe8e8ba
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-18 23:44:13 -0400

    Exercise - Conditional Statement

--- 03-control-flow/exercise-fizzbuzz/src/com/martinez/Main.java
+++ 03-control-flow/exercise-fizzbuzz/src/com/martinez/Main.java
@@ -12,5 +12,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Number: ");
         int number = scanner.nextInt();
+
+        if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("buzz");
+        else if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("Fizzbuzz");
+        else
+            System.out.println(number);
     }
 }
