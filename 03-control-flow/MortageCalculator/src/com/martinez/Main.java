90ecbe47b9cc5a46dae198e9e185bd65ae534802
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-20 00:38:46 -0400

    Mortage - moved principal declartion outside of scope so it can be used

--- 03-control-flow/MortageCalculator/src/com/martinez/Main.java
+++ 03-control-flow/MortageCalculator/src/com/martinez/Main.java
@@ -48,12 +48,14 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         // Principal
         while (true) {
             System.out.print("Principal/Loan: ");
-            int principal = scanner.nextInt();
+            principal = scanner.nextInt();
             if (principal >= 1000 && principal <= 1_000_000)
                 break;
             System.out.println("Enter a number between 1,000 and 1,000,000 please.");
