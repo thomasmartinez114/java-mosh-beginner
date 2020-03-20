f98bd041a35893a66d44be70685f50e4be079daf
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-20 00:43:51 -0400

    Mortage - while (true) for interest

--- 03-control-flow/MortageCalculator/src/com/martinez/Main.java
+++ 03-control-flow/MortageCalculator/src/com/martinez/Main.java
@@ -47,8 +47,8 @@ public class Main {
 
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
-
         int principal = 0;
+        float annualInterest = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -58,14 +58,19 @@ public class Main {
             principal = scanner.nextInt();
             if (principal >= 1000 && principal <= 1_000_000)
                 break;
-            System.out.println("Enter a number between 1,000 and 1,000,000 please.");
+            System.out.println("Enter a number between 1,000 - 1,000,000.");
         }
-        // Interest Rate
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        // System.out.println(monthlyInterest);
 
+        // Interest Rate
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30)
+                float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+        }
+            System.out.println("Enter a number between 1 - 30.")
+        }
         // Period (Years)
         System.out.print("Length of Loan (Years): ");
         byte years = scanner.nextByte();
