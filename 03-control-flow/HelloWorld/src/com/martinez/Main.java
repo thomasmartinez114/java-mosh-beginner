a6762a01b3098a5fb2aedd6e6a2c422eb6eaa66a
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-19 23:21:49 -0400

    Loops - Foreach

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -5,19 +5,9 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// While loops better when don't know how many times you want loop to run
+		String[] fruits = { "Apple", "Mango", "Orange" };
 
-		// Ask for user input until they input quit
-		Scanner scanner = new Scanner(System.in);
-		String input = "";
-		while (true) {
-			System.out.print("Input: ");
-			input = scanner.next().toLowerCase();
-			if (input.equals("pass"))
-				continue;
-			if (input.equals("quit"))
-				break;
-			System.out.println(input);
-		}
+		for (int i = 0; i < fruits.length; i++)
+			System.out.println(fruits[i]);
     }
 }
