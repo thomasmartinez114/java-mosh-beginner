7a824005e21f06f112594ecf4d686fb8739f34f5
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-19 22:12:44 -0400

    Loops - For - printing with i subtracting

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,7 @@ package com.martinez;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i <= 5; i++)
+		for (int i = 5; i > 0; i--)
 			System.out.println("Hello World " + i );
     }
 }
