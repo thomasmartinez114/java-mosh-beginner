30da5b86e48d3b659a64482a41e23600d2184ecb
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-19 22:12:09 -0400

    Loops - For - printing with i

--- 03-control-flow/HelloWorld/src/com/martinez/Main.java
+++ 03-control-flow/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,7 @@ package com.martinez;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++)
-			System.out.println("Hello World");
+		for (int i = 0; i <= 5; i++)
+			System.out.println("Hello World " + i );
     }
 }
