c00bf182a48af17bf51b3e58ada6d3d50c774db9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-21 18:25:00 -0400

    Extracting Methods - Refactored - into calculateMortage

--- 04-clean-coding/MortageCalculator/src/com/martinez/Main.java
+++ 04-clean-coding/MortageCalculator/src/com/martinez/Main.java
@@ -15,46 +15,12 @@ import java.text.NumberFormat;
 public class Main {
 
     public static void main(String[] args) {
-        /* Testing Data
-        // Static inputs for now then test
-        // Testing data
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        final int PRINCIPAL = 100_000;
-        final float ANNUAL_INTEREST = 5.5F;
-        final int PERIOD_YEARS = 30;
-        // Monthly Interest
-        float monthlyInterest = ANNUAL_INTEREST / PERCENT / MONTHS_IN_YEAR;
-        // System.out.println("Monthly Interest rate is: " + monthlyInterest);
-
-        // # of Payments = PERIOD_YEARS * MONTHS_IN_YEAR
-        int numberPayment = PERIOD_YEARS * MONTHS_IN_YEAR;
-        // System.out.println("Total Payments through loan: " + numberPayment);
-
-        // Mortage
-        double mortage = PRINCIPAL
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberPayment)
-                / (Math.pow(1 + monthlyInterest, numberPayment) - 1));
-        // System.out.println("Mortage bill is: " + mortage);
-
-        // Format the mortage into currency
-        String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
-        System.out.println("Mortage Payment is: " + mortageFormatted);
-        */
-
-        //////////////////////////////////////////////////////////
-        // User input
-
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         int principal = 0;
         float monthlyInterest = 0;
         int numPayments = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        // Principal
         while (true) {
             System.out.print("Principal/Loan: ");
             principal = scanner.nextInt();
@@ -63,34 +29,25 @@ public class Main {
             System.out.println("Enter a number between 1,000 - 1,000,000.");
         }
 
-        // Interest Rate
         while (true) {
             System.out.print("Annual Interest Rate: ");
             float annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <= 30) {
-                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            if (annualInterest >= 1 && annualInterest <= 30)
                 break;
-        }
             System.out.println("Enter a number between 1 - 30.");
         }
 
-        // Period (Years) 
         while (true) {
         System.out.print("Length of Loan (Years): ");
         byte years = scanner.nextByte();
-        if (years >= 1 && years <= 30) {
-            numPayments = MONTHS_IN_YEAR * years;
+        if (years >= 1 && years <= 30)
             break;
-        }
+
         System.out.println("Enter a number between 1 - 30.");
     }
 
-        // Mortage Rate
-        double mortage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numPayments)
-                / (Math.pow(1 + monthlyInterest, numPayments) - 1));
+        calculateMortage(principal, annualInterest, years);
 
-        // Format the Mortage into currency
         String formattedMortage = NumberFormat.getCurrencyInstance().format(mortage);
         System.out.println("Mortage is: " + formattedMortage);
     }
@@ -99,6 +56,17 @@ public class Main {
             int principal,
             float annualInterest,
             byte years) {
-        
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numPayments = MONTHS_IN_YEAR * years;
+
+        double mortage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numPayments)
+                / (Math.pow(1 + monthlyInterest, numPayments) - 1));
+
+        return mortage;
     }
 }
