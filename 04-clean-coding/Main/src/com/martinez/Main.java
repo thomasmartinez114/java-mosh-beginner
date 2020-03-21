a75b4d99af639bb6db7a98a443caa7c880e25959
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-21 16:25:55 -0400

    Methods - Created greetUser()

---
+++ 04-clean-coding/Main/src/com/martinez/Main.java
@@ -0,0 +1,12 @@
+package com.martinez;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    greetUser("Thomas");
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello " + name);
+    }
+}
